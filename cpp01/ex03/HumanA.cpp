6eae4cb00eda4ccfc029090bc4541b182392b774
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-16 03:24:13 +0700

    ex03 no leaks and clean

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -10,5 +10,5 @@ HumanA::~HumanA(void) {}
 
 void	HumanA::attack(void)
 {
-	std::cout << this->_name << "attack by" << this->_weapon->getType() << std::endl;
+	std::cout << this->_name << " attack by" << this->_weapon->getType() << std::endl;
 }
