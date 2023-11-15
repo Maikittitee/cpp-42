4faf4e4dc30a2d8d5443862a93d9261ac5643d4f
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-16 01:55:21 +0700

    ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+
+	std::cout << "address of str:\t"<< &str << std::endl; 
+	std::cout << "stringPTR held addr:\t"<< stringPTR << std::endl; 
+	std::cout << "stringREF held addr:\t"<< &stringREF << std::endl;
+
+	std::cout << "The value of the string variable:\t" << str <<std::endl;
+	std::cout << "The value pointed to by stringPTR:\t" << *stringPTR <<std::endl;
+	std::cout << "value of str:\t" << stringREF <<std::endl;
+
+}
