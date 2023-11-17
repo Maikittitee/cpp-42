2f489e0e351becaaff5c653009c3782e0fdb65b3
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-17 16:12:12 +0700

    clean

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int	main(int argc, char *argv[])
+{
+	std::fstream file;
+	std::string		line;
+	std::string		newline;
+	int				i = 0;
+	if (argc == 4)
+	{
+		file.open(argv[1], std::ios::in | std::ios::out);
+		if (file.is_open())
+		{
+			while (getline(file, line))
+				newline.append(line);
+		}
+		file.close();
+		file.open(argv[1], std::ios::out | std::ios::trunc);
+		while (newline[i])
+		{
+			if (newline.compare(i, std::string(argv[2]).length(), argv[2]) == 0)
+			{
+				file << argv[3];
+				i += std::string(argv[2]).length();
+				continue ;
+			}
+			else
+				file << newline[i];
+			i++;
+		}
+		// file << std::endl;
+		file.close();
+	}
+}
