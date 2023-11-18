d3e9d5ea36ce7bea50d165b0866b672f41bff1bf
ktunchar <maikittitee1615@gmail.com>
2023-11-19 01:44:31 +0700

    ex06 done with tested

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,41 +6,47 @@ Harl::Harl( void )
 	this->_level[1] = "INFO";
 	this->_level[2] = "WARNING";
 	this->_level[3] = "ERROR";
-	this->func_ptr[0] = &(Harl::debug);
-	this->func_ptr[1] = &(Harl::info);
-	this->func_ptr[2] = &(Harl::warning);
-	this->func_ptr[3] = &(Harl::error);
+	this->func_ptr[0] = &Harl::debug;
+	this->func_ptr[1] = &Harl::info;
+	this->func_ptr[2] = &Harl::warning;
+	this->func_ptr[3] = &Harl::error;
 }
+
+Harl::~Harl( void ) {}
 
 void	Harl::debug( void )
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+
 }
 
 void	Harl::info( void )
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void )
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error( void )
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complainFilter ( std::string level )
 {
-	int	mode;
-
+	int mode;
+	
 	for (mode = 0; mode < 4; mode++){
 		if (level.compare(this->_level[mode]) == 0)
 			break;
 	}
-
 	switch (mode){
 		case 0:
 			(this->*func_ptr[0])();
@@ -50,7 +56,9 @@ void	Harl::complainFilter ( std::string level )
 			(this->*func_ptr[2])();
 		case 3:
 			(this->*func_ptr[3])();
+			break;
 		default:
-			std::cerr << "Error" << std::endl;
+			std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+			break;
 	}
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -2,5 +2,10 @@
 
 int	main(int ac, char **av)
 {
-	
+	Harl harl;
+	if (ac != 2)
+		return (1);
+
+	harl.complainFilter(av[1]);
+	return (0);
 }
