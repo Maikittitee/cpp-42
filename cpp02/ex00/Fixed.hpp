fe4fab5c5f3fef9caec834bd2664c29e6db74181
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-21 02:27:49 +0700

    ex00 is done

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,8 +1,21 @@
-class Fixed
-{
-   public:
-      Fixed();
-      Fixed(const A &a);
-      ~Fixed();
-      Fixed & operator = (const A &a);
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed{
+	public:
+		Fixed();
+		Fixed(const Fixed &fp);
+		~Fixed();
+		Fixed & operator = (const Fixed &fp);
+		int	getRawBits( void ) const ;
+		void	setRawBits( int const raw);
+	private:
+		int					_raw_bits;
+		static const int	_op_bits = 8;
 };
+
+
+
+#endif
