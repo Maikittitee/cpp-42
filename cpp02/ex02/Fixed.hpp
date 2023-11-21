1a14a02e1914591596240812486298f616b39c06
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-21 16:39:08 +0700

    copy for ex02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed{
+	public:
+		Fixed();
+		Fixed(const Fixed &fp);
+		Fixed(const int n);
+		Fixed(const float n);
+		~Fixed();
+		Fixed & operator = (const Fixed &fp);
+		int		getRawBits( void ) const ;
+		void	setRawBits( int const raw);
+		int		toInt( void ) const ;
+		float	toFloat( void ) const ;
+	private:
+		int					_raw_bits;
+		static const int	_op_bits = 8;
+};
+
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fp);
+
+#endif
