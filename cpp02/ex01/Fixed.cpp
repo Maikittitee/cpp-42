a353cd08414fd06162e7e2c0ecf79f8d83ef2609
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-11-21 16:37:01 +0700

    ex01

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,20 +1,32 @@
 #include "Fixed.hpp"
 
+// Constructor and Destructor
+
 Fixed::Fixed() :_raw_bits(0) 
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
-}
 
 Fixed::Fixed(const Fixed &fp)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	*this = fp;
-	// this->_raw_bits = fp.getRawBits();
+	// *this = fp; // is it the same address ? it shouldn't be
+	
+	// alternative
+	this->_raw_bits = fp.getRawBits();
 }
+
+Fixed::Fixed(const int n): _raw_bits(n << _op_bits) {}
+
+Fixed::Fixed(const float n): _raw_bits(roundf(n * (1 << this->_op_bits))) {}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+// Oparator 
 
 Fixed & Fixed::operator = (const Fixed &fp)
 {
@@ -24,6 +36,8 @@ Fixed & Fixed::operator = (const Fixed &fp)
 
 }
 
+
+// Member Function
 
 int	Fixed::getRawBits() const
 {
@@ -35,4 +49,22 @@ int	Fixed::getRawBits() const
 void Fixed::setRawBits(int const raw)
 {
 	this->_raw_bits = raw;
+}
+
+int	Fixed::toInt( void ) const
+{
+	return (this->_raw_bits >> this->_op_bits);
+
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return (float(this->_raw_bits) / (1 << this->_op_bits));
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fp) 
+{
+    os << (float)fp.toFloat();
+    return os;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -7,15 +7,20 @@ class Fixed{
 	public:
 		Fixed();
 		Fixed(const Fixed &fp);
+		Fixed(const int n);
+		Fixed(const float n);
 		~Fixed();
 		Fixed & operator = (const Fixed &fp);
-		int	getRawBits( void ) const ;
+		int		getRawBits( void ) const ;
 		void	setRawBits( int const raw);
+		int		toInt( void ) const ;
+		float	toFloat( void ) const ;
 	private:
 		int					_raw_bits;
 		static const int	_op_bits = 8;
 };
 
 
+std::ostream& operator<<(std::ostream& os, const Fixed& fp);
 
 #endif
