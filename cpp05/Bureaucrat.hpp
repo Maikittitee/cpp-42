e313d4370dfa3808655597049ac9e6611f95db3f
ktunchar <maikittitee1615@gmail.com>
2024-03-08 12:16:43 +0700

    chore(Bureaucrat): hpp prototype

---
+++ cpp05/Bureaucrat.hpp
@@ -0,0 +1,31 @@
+#ifndef BUREAUCRAT_HPP
+#   define BUREAUCRAT_HPP
+
+#   include <iostream>
+#   include <string>
+
+class Bureaucrat    {
+	private:
+		const std::string	_name;
+		int					_grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name);
+		Bureaucrat(const Bureaucrat &other);
+
+		Bureaucrat operator= (const Bureaucrat &rhs);
+		
+
+		int get_grade( void );
+		const std::string get_name( void );
+
+		int set_grade( int grade );
+		const std::string set_name( const std::string name );
+
+
+
+}
+
+
+#endif
