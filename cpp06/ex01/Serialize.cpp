fab6fb13e5827a2c09350dffb387bcb983b1fc63
ktunchar <maikittitee1615@gmail.com>
2024-07-29 15:26:46 +0700

    protect null in serialize

--- cpp06/ex01/Serialize.cpp
+++ cpp06/ex01/Serialize.cpp
@@ -33,11 +33,15 @@ std::ostream &			operator<<( std::ostream & o, Serialize const & i )
 
 uintptr_t  Serialize::serialize(Data* ptr)
 {
+	if (!ptr)
+		return (0);
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data* Serialize::deserialize(uintptr_t raw)
 {
+	if (!raw)
+		return (NULL);
 	return reinterpret_cast<Data *>(raw);
 
 }
