8f3596cc29929363f3ca8ee57d5ba9e51a442316
ktunchar <maikittitee1615@gmail.com>
2024-08-12 00:42:58 +0700

    write main test

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,10 +1,36 @@
 #include "Array.hpp" 
 #include <iostream>
 
+
 int	main() {
 	Array<int> arr(10);
+	int *checker = new int[10];
 
-	arr[0] = 99;
-	std::cout << arr.size() << std::endl; 
-	std::cout << arr[0] << std::endl; 
+	// test assign
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		const int value = rand();
+		arr[i] = value;
+		checker[i] = value;
+	}
+
+	// test print
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Arr: " << arr[i] << ", Checker: " << checker[i] << std::endl; 
+	}
+
+	// test exception
+	try {
+		std::cout << arr[10] << std::endl;
+	} catch (const std::exception& e){
+		std::cout << e.what() << std::endl;
+
+	}
+
+
+	delete [] checker;
+
+
 }
