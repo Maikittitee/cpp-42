084184b9d5eb318698e4f7bf9384a024588f70f1
ktunchar <maikittitee1615@gmail.com>
2024-08-27 19:05:55 +0700

    feat: normal merge sort

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,1 +1,34 @@
-fdsfkshl
+#include "PmergeMe.hpp"
+
+
+void	MergeSort(std::vector<int> &vec, size_t start, size_t end){
+	if (start < end){
+		size_t middle = (start + end) / 2;
+		MergeSort(vec, start, middle);
+		MergeSort(vec, middle + 1, end);
+		Merge(vec, start, middle, end);
+	}
+}
+
+void	Merge(std::vector<int> &vec, size_t start, size_t middle, size_t end){
+	std::vector<int> left(vec.begin() + start, vec.begin() + middle + 1);
+	std::vector<int> right(vec.begin() + middle + 1, vec.begin() + end + 1);
+	
+	std::vector<int>::iterator it1 = left.begin();
+	std::vector<int>::iterator it2 = right.begin();
+
+
+	for (size_t i = start; i < end + 1; i++){
+		if (it1 == left.end() && it2 != right.end())
+			vec[i] = *(it2++);
+		if (it1 != left.end() && it2 == right.end())
+			vec[i] = *(it1++);
+		if (*it1 < *it2)
+			vec[i] = *(it1++);
+		else 
+			vec[i] = *(it2++);
+		i++;
+		
+	}
+
+}
---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,14 @@
+#ifndef PMERGEME_HPP
+#	define PMERGEME_HPP
+
+#	include <iostream>
+#	include <vector>
+
+
+
+
+void	MergeSort(std::vector<int> &vec, size_t start, size_t end);
+void	Merge(std::vector<int> &vec, size_t start, size_t middle, size_t end);
+
+
+#endif
