21b4109e4b63105c412313edcf047be8dd9bcd9d
ktunchar <maikittitee1615@gmail.com>
2024-08-28 01:43:40 +0700

    chore: clean Merge function

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -18,27 +18,15 @@ void	Merge(std::vector<int> &vec, size_t start, size_t middle, size_t end){
 	std::vector<int>::iterator it1 = left.begin();
 	std::vector<int>::iterator it2 = right.begin();
 
-
 	for (size_t i = start; i < end + 1; i++){
-		std::cout << "it1: " << *it1 << ", it2: " << *it2 << std::endl;
-		if (it1 == left.end() && it2 != right.end()){
-			vec[i] = *(it2);
-			it2++;
-		}
-		else if (it1 != left.end() && it2 == right.end()){
-			vec[i] = *(it1);
-			it1 ++;
-
-		}
-		else if (*it1 < *it2){
-			vec[i] = *(it1);
-			it1++;
-		}
-		else {
-			vec[i] = *(it2);
-			it2++;
-		}
-		// std::cout << "now vec index" << i << " => " << vec[i] << std::endl;	
+		if (it1 == left.end() && it2 != right.end())
+			vec[i] = *(it2++);
+		else if (it1 != left.end() && it2 == right.end())
+			vec[i] = *(it1++);
+		else if (*it1 < *it2)
+			vec[i] = *(it1++);
+		else
+			vec[i] = *(it2++);
 	}
 
 }
